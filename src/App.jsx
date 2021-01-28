752b85b86d9805d52ca751e826fa0684bd7deccc
Matheus <matheusssb@ufmg.br>
2021-01-28 12:03:46 -0300

    FEAT: Botão de alterar tema

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <ThemeContextProvider>
       <div className="teste" />
-      matheus
     </ThemeContextProvider>
   );
 }
--- src/Context/ThemeContext.jsx
+++ src/Context/ThemeContext.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import Switch from 'react-switch';
 
 const themes = {
   technologic: {
-    primary: '#4292C6',
     backgroundColor: '#233643',
-    color: '#FFF',
+    fontColor: '#FFF',
     headerBackground: '#1B2126',
     dashboardBackground: '#1B2126',
     componentsBackgroud: '#0E1317',
@@ -16,25 +16,23 @@ const themes = {
     graphColor6: '#08306B',
   },
   coffee: {
-    primary: '#E8A900',
     backgroundColor: '#4D2914',
-    color: '#FFF',
-    headerbackground: '#190902',
-    dashboardbackground: '#DEA779',
-    componentscolor: '#E8A900',
-    componentsbackgroud: '#2F1608',
-    graphColor1: '#DEEBF7',
-    graphColor2: '#C6DBEF',
-    graphColor3: '#9ECAE1',
-    graphColor4: '#4292C6',
-    graphColor5: '#2171B5',
-    graphColor6: '#08306B',
+    fontColor: '#FFF',
+    headerBackground: '#190902',
+    dashboardBackground: '#DEA779',
+    componentsBackgroud: '#2F1608',
+    graphColor1: '#E8A900',
+    graphColor2: '#E3A841',
+    graphColor3: '#DEA779',
+    graphColor4: '#956746',
+    graphColor5: '#4D2914',
+    graphColor6: '#190902',
   },
 };
 const ThemeContext = React.createContext({});
 
 function ThemeContextProvider({ children }) {
-  const [themeName, setThemeName] = useState('coffee');
+  const [themeName, setThemeName] = useState('technologic');
   const [theme, setTheme] = useState(themes[themeName]);
 
   const setCSSVariables = (themeAux) => {
@@ -72,6 +70,19 @@ function ThemeContextProvider({ children }) {
 
   return (
     <ThemeContext.Provider value={{ toggleTheme, themeName }}>
+      <Switch
+        onChange={toggleTheme}
+        checked={themeName === 'technologic'}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        height={10}
+        width={40}
+        handleDiameter={20}
+        offColor="#E8A900"
+        onColor="#2171B5"
+        onHandleColor="#1B2126"
+        offHandleColor="#1B2126"
+      />
       {children}
     </ThemeContext.Provider>
   );

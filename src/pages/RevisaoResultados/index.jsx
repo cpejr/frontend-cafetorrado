0835adc88f1aa87fa1013e17af9194f529eca5d0
Talles Marques <tallesemanuel1@gmail.com>
2021-02-10 01:17:25 -0300

    Feat: Adiciona página de revisão de resultados em branco

---
+++ src/pages/RevisaoResultados/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RevisaoResultados = () => {
+  return <h1>Bom dia</h1>;
+};
+
+export default RevisaoResultados;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Teste from './Components/teste';
+import RevisaoResultados from './pages/RevisaoResultados';
 
 const RouterComponent = () => {
   return (
@@ -9,6 +10,8 @@ const RouterComponent = () => {
         <Route path="/" exact>
           <h1>Página Home</h1>
           <Link to="/teste">Ir para teste</Link>
+          <br />
+          <Link to="/revisaoresult">Ir para revisão de resultados</Link>
         </Route>
 
         <Route path="/teste">
@@ -16,6 +19,8 @@ const RouterComponent = () => {
           <h1>Página de testes</h1>
           <Link to="/">Ir para Home</Link>
         </Route>
+
+        <Route path="/revisaoresult" component={RevisaoResultados} />
       </Switch>
     </Router>
   );

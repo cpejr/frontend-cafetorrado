8338456cd5dd6b3c16c7a549bf849cba829a83fc
Talles Marques <tallesemanuel1@gmail.com>
2021-02-10 03:48:51 -0300

    Feat: Adiciona novo drawer

--- src/components/organisms/drawer/index.js
+++ src/components/organisms/drawer/index.js
@@ -1,9 +1,15 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
+import InsertChartIcon from '@material-ui/icons/InsertChart';
+import Divider from '../../atoms/divider';
 
 import { DrawerMenu, DrawerHeader, OutrasInformacoes } from './insideDrawer';
 import { useToggle } from '../../../hooks';
@@ -64,40 +70,118 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function MiniDrawer({ children, valuesInfo }) {
-  const classes = useStyles();
-  const [open, drawerToggle] = useToggle(true);
+// export default function MiniDrawer({ children, valuesInfo }) {
+//   const classes = useStyles();
+//   const [open, drawerToggle] = useToggle(true);
 
+//   return (
+//     <div className={classes.root}>
+//       <CssBaseline />
+
+//       <AppBar
+//         position="fixed"
+//         className={clsx(classes.appBar, {
+//           [classes.appBarShift]: open,
+//         })}
+//       />
+
+//       <Drawer
+//         variant="permanent"
+//         className={`drawer ${clsx(classes.drawer, {
+//           [classes.drawerOpen]: open,
+//           [classes.drawerClose]: !open,
+//         })}`}
+//         classes={{
+//           paper: clsx({
+//             [classes.drawerOpen]: open,
+//             [classes.drawerClose]: !open,
+//           }),
+//         }}
+//       >
+//         <DrawerHeader drawerToggle={drawerToggle} />
+//         <DrawerMenu drawerOpen={open} />
+//         <OutrasInformacoes drawerOpen={open} valuesInfo={valuesInfo} />
+//       </Drawer>
+
+//       <main className={`${classes.content} content`}>{children}</main>
+//     </div>
+//   );
+// }
+
+const DrawerComponent = ({ valuesInfo }) => {
+  const [open, toggle] = useToggle(true);
   return (
-    <div className={classes.root}>
-      <CssBaseline />
+    <div
+      className={
+        open ? 'drawer-content' : 'drawer-content drawer-content-close'
+      }
+    >
+      <div className="app-bar-content">
+        <button type="button" className="app-bar" onClick={toggle}>
+          <MenuIcon />
+          <span>Menu</span>
+        </button>
+      </div>
+      <Divider />
 
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      />
+      <div className="menu">
+        <div className="link-menu" href="/">
+          <SearchIcon />
+          <span>Selecionar Receita</span>
+        </div>
+        <Divider />
 
-      <Drawer
-        variant="permanent"
-        className={`drawer ${clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}`}
-        classes={{
-          paper: clsx({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <DrawerHeader drawerToggle={drawerToggle} />
-        <DrawerMenu drawerOpen={open} />
-        <OutrasInformacoes drawerOpen={open} valuesInfo={valuesInfo} />
-      </Drawer>
+        <div className="link-menu" href="/">
+          <AddToPhotosIcon />
+          <span>Adicionar Receita</span>
+        </div>
+        <Divider />
 
-      <main className={`${classes.content} content`}>{children}</main>
+        <div className="link-menu" href="/">
+          <InsertChartIcon />
+          <span>Monitoramento</span>
+        </div>
+
+        {open && (
+          <div className="monitoramento-opcoes">
+            <form>
+              <div className="radio-group">
+                <label>
+                  <input type="radio" name="automatico" />
+                  Automatico
+                </label>
+              </div>
+
+              <br />
+
+              <div className="radio-group">
+                <label>
+                  <input type="radio" name="manual" />
+                  Manual
+                </label>
+              </div>
+            </form>
+          </div>
+        )}
+        <Divider />
+      </div>
+
+      {open && (
+        <div className="outras-infos">
+          <h1>Mais Informações:</h1>
+          <p>
+            Pressão: <span>{valuesInfo.pressao} atm</span>
+          </p>
+          <p>
+            Umidade: <span>{valuesInfo.umidade}%</span>
+          </p>
+          <p>
+            Massa de Grãos: <span>{valuesInfo.massaGraos}g</span>
+          </p>
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default DrawerComponent;
--- src/templates/index.js
+++ src/templates/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import DrawerComponent from '../components/organisms/drawer';
 import HeaderComponent from '../components/organisms/header';
+import './styles.css';
 
 const TemplateWithDrawer = ({ children, valuesInfo }) => {
   return (
-    <DrawerComponent valuesInfo={valuesInfo}>
-      <HeaderComponent />
-      {children}
-    </DrawerComponent>
+    <div className="body">
+      <DrawerComponent valuesInfo={valuesInfo} />
+
+      <div className="page">
+        {/* <HeaderComponent /> */}
+        {children}
+      </div>
+    </div>
   );
 };
 

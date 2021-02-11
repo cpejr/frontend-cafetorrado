8d340f54bccd469eb348216d96fe8ce76291cf32
Igor Amoras <igorduarteamoras@gmail.com>
2021-02-11 11:26:35 -0300

    Feat: Streaming entre back e front
    Os dados atualizam a cada segundo

--- src/index.jsx
+++ src/index.jsx
@@ -1,51 +1,27 @@
 import React, { Component, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { w3cwebsocket as W3CWebSocket } from 'websocket';
 import TemplateWithDrawer from './templates';
-// import { w3cwebsocket as W3CWebSocket } from 'websocket';
 import App from './App';
-/*
+import './index.css';
 
+const io = require('socket.io-client');
 
-const client = new W3CWebSocket('ws://127.0.0.1:9000');
-
-const valuesInfo = {
-  pressao: 8.9,
-  umidade: 79,
-  massaGraos: 2468,
+const socket = io.connect('http://localhost:8888');
+let newDetails = {
+  waterTemp: 0,
+  fireTemp: 0,
 };
-
-export default function App() {
-  useEffect(() => {
-    client.onopen = () => {
-      console.log('Websocket Client conected');
+console.log(newDetails.waterTemp, newDetails.fireTemp);
+socket.on('connect', (data) => {
+  socket.on('newData', (_data) => {
+    newDetails = {
+      waterTemp: _data.waterTemp,
+      fireTemp: _data.fireTemp,
     };
-    client.onmessage = (message) => {
-      const dataFromServer = JSON.parse(message.data);
-      console.log('got reply!', dataFromServer);
-    };
-  }, []);
+    console.log(newDetails.waterTemp, newDetails.fireTemp);
+  });
 
-  const onButtonClicked = (value) => {
-    client.send(
-      JSON.stringify({
-        type: 'message',
-        msg: value,
-      })
-    );
-  };
+  console.log('I"m alive, I breathe and I have a purpose for existing');
+});
 
-  return (
-    <TemplateWithDrawer valuesInfo={valuesInfo}>
-      <h1>Apenas um teste</h1>
-      <h2>Outro teste</h2>
-      <button type="button" onClick={() => onButtonClicked('hello')}>
-        {' '}
-        Send message{' '}
-      </button>
-    </TemplateWithDrawer>
-  );
-}
-*/
 ReactDOM.render(<App />, document.getElementById('root'));

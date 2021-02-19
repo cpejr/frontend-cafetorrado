2e9c9d696e62d1608fbce8bc4a805bb1a507977a
Talles Marques <tallesemanuel1@gmail.com>
2021-02-19 10:40:11 -0300

    Feat: Grafico de linha

--- src/components/RevisaoGraph/index.jsx
+++ src/components/RevisaoGraph/index.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
 
-const RevisaoGraph = () => {
-  return <h1>Bom dia</h1>;
+const RevisaoGraph = ({ data }) => {
+  return (
+    <div style={{ height: '50%', width: '100%' }}>
+      <Line
+        data={data}
+        options={{
+          responsive: true,
+          // title: { text: ' Tempo de torra ', display: true },
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  autoSkip: true,
+                  maxTicksLimit: 100,
+                  beginAtZero: true,
+                },
+                gridLines: {
+                  display: false,
+                },
+              },
+            ],
+            xAxes: [
+              {
+                gridLines: { display: false },
+              },
+            ],
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default RevisaoGraph;

d36dfb40c1d14fea240b926e7d8702d8f6fc36ab
Matheus <matheusssb@ufmg.br>
2021-02-25 09:23:37 -0300

    Fix: Animação nas rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import ResultsRevision from './Pages/ResultsRevision';
 import Teste from './components/theme';
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 import TemplateWithDrawer from './Templates/index';
 import Automatic from './Pages/AutomaticPage/Automatic';
 import Manual from './Pages/ManualPage/manual';
@@ -23,36 +24,53 @@ const RouterComponent = ({ data }) => {
   return (
     <Router>
       <TemplateWithDrawer valuesInfo={valuesInfo}>
-        <Switch >
-          <Route path="/" exact>
-            <h1>Página Home</h1>
-            <Link to="/teste">Ir para teste</Link>
+        <Route render={({location}) => (
+<div>
+<TransitionGroup>
+<CSSTransition
+key={location.key}
+timeout={300}
+classNames='fade'>
+<Switch location={location}>
+<Route path="/" exact>
+<h1>Página Home</h1>
+<Link to="/teste">Ir para teste</Link>
 
-            <br />
-            <Link to="/ResultsRevision">Ir para Revisão de Resultados</Link>
-            <br />
-            <Link to="/automatic">Ir para Tela Automático</Link>
-            <br />
-            <Link to="/Manual">Ir para Tela Manual</Link>
-          </Route>
+<br />
+<Link to="/ResultsRevision">Ir para Revisão de Resultados</Link>
+<br />
+<Link to="/automatic">Ir para Tela Automático</Link>
+<br />
+<Link to="/Manual">Ir para Tela Manual</Link>
+</Route>
 
-          <Route path="/automatic" >
-            <Automatic data={data} />
-          </Route>
-          <Route path="/manual">
-            <Manual data={data} />
-          </Route>
 
-          <Route path="/ResultsRevision" component={ResultsRevision} >
-            <ResultsRevision data={data}/>
-          </Route>
 
-          <Route path="/teste">
-            <Teste />
-            <h1>Página de testes</h1>
-            <Link to="/">Ir para Home</Link>
-          </Route>
-        </Switch>
+<Route path="/automatic" >
+<Automatic data={data} />
+</Route>
+<Route path="/manual">
+<Manual data={data} />
+</Route>
+
+
+<Route path="/ResultsRevision" component={ResultsRevision} >
+<ResultsRevision data={data}/>
+</Route>
+
+<Route path="/teste">
+<Teste />
+<h1>Página de testes</h1>
+<Link to="/">Ir para Home</Link>
+</Route>
+</Switch>
+</CSSTransition>
+</TransitionGroup>
+</div>
+)}/>
+
+          
+        
    
       </TemplateWithDrawer>
     </Router>

788cb226e1ad4e74937cc67c0af3eddbaa91dfdb
Igor Amoras <igorduarteamoras@gmail.com>
2021-02-25 18:44:00 -0300

    Fix: mudanças finais

--- src/components/Buttons/ButtonsAdjustments/ButtonAdjustment.jsx
+++ src/components/Buttons/ButtonsAdjustments/ButtonAdjustment.jsx
@@ -11,11 +11,16 @@ function ButtonAdjustment(){
   const [skinButton, setSkinButton] = useState(skins.s12)
 
   const knobstyle = {
-    width: "80px",
-    height: "80px"
+    width: "120px",
+    height: "120px"
   };
-  function sendData(){socket.emit('dataFromButton', (Math.round(buttonValue*100)/100000))}
-  function changeValue(val) {setButtonValue(val)}
+  function sendData(){
+    socket.emit('dataFromButton', (Math.round(buttonValue*100/1000)/100))
+  }
+  function changeValue(val) {
+    if(Math.abs(val-buttonValue) < 200)
+      setButtonValue(val)
+  }
     return(
     <div>
       <Knob id = "myKnob" 

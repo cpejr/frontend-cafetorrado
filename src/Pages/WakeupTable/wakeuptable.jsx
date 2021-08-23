1512b3dae27bfc7aac919bd79b90a663e446308d
Pedro Babosa <pedrobarbosa@cpejr.com.br>
2021-08-23 02:04:49 -0300

    Restrição de upload de arquivo para somente do tipo binário

--- src/Pages/WakeupTable/wakeuptable.jsx
+++ src/Pages/WakeupTable/wakeuptable.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import Modal from '@material-ui/core/Modal';
 import './wakeuptable.css';
+import { FiAlertCircle } from 'react-icons/fi';
 
 function wakeuptable() {
   const [file, setFile] = useState();
@@ -9,7 +10,12 @@ function wakeuptable() {
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
-    setOpen(true);
+    if (fileName.split('.').pop() === 'bin') {
+      setOpen(true);
+    } else {
+      setOpen(false);
+      alert('Formato de arquivo inadequado!');
+    }
   };
 
   const handleClose = () => {
@@ -23,23 +29,25 @@ function wakeuptable() {
 
   const uploadFile = async (e) => {
     const formData = new FormData();
+    // Relação atributo-valor
     formData.append('file', file);
     formData.append('fileName', fileName);
-    try {
-      await axios.post(
-        'http://localhost:8888/upload',
-        formData,
-      );
-    } catch (ex) {
-      console.error(ex);
-    }
+    // try {
+    //   await axios.post(
+    //     'http://localhost:8888/upload',
+    //     formData,
+    //   );
+    // } catch (ex) {
+    //   console.error(ex);
+    // }
   };
 
   return (
     <div className="WakeContainer">
       <div className="Input-Button">
-        <h1> Envie seu Arquivo </h1>
-        <input className="Input" type="file" onChange={saveFile} />
+        <h1> Envie seu Arquivo binário </h1>
+        {/* // Definição do tipo de arquivo permitido para upload */}
+        <input className="Input" type="file" accept=".bin" onChange={saveFile} />
         <button className="Button" type="button" onClick={handleOpen}>
           Fazer Upload
         </button>

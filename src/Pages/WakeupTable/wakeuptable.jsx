8afaefd45e38cb4c1f3aabcb8cf5b773d31b4a76
Gabriel Barros Mauad Santos <gabrielbms711@gmail.com>
2021-09-08 17:30:47 -0300

    correções PR

--- src/Pages/WakeupTable/wakeuptable.jsx
+++ src/Pages/WakeupTable/wakeuptable.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import Modal from '@material-ui/core/Modal';
 import './wakeuptable.css';
-import { FiAlertCircle } from 'react-icons/fi';
 
 function wakeuptable() {
   const [file, setFile] = useState();
@@ -29,24 +27,14 @@ function wakeuptable() {
 
   const uploadFile = async (e) => {
     const formData = new FormData();
-    // Relação atributo-valor
     formData.append('file', file);
     formData.append('fileName', fileName);
-    // try {
-    //   await axios.post(
-    //     'http://localhost:8888/upload',
-    //     formData,
-    //   );
-    // } catch (ex) {
-    //   console.error(ex);
-    // }
   };
 
   return (
     <div className="WakeContainer">
       <div className="Input-Button">
         <h1> Envie seu Arquivo binário </h1>
-        {/* // Definição do tipo de arquivo permitido para upload */}
         <input className="Input" type="file" accept=".bin" onChange={saveFile} />
         <button className="Button" type="button" onClick={handleOpen}>
           Fazer Upload

0ee413c51028063ee49cd3faa3d88c98320e1a21
Emanuella Ferraz <manufeitalferraz@gmail.com>
2021-09-14 16:45:46 -0300

    começo de implementação dos inputs dos marcadores

--- src/Pages/ResultsRevision/index.jsx
+++ src/Pages/ResultsRevision/index.jsx
@@ -6,6 +6,24 @@ import { MainGraph } from '../../components/MainGraph/MainGraph';
 import { deleteLastRoast } from '../../components/Functions/RequestHandler/RequestHandler';
 import getChartParams from '../../components/Functions/getChartParams';
 
+const [mark1, setmark1] = useState();
+const [mark2, setmark2] = useState();
+const [mark3, setmark3] = useState();
+const [mark4, setmark4] = useState();
+const [mark5, setmark5] = useState();
+
+useEffect(() => {
+  setmark1(marcador1);
+  setmark2(marcador2);
+  setmark3(marcador3);
+  setmark4(marcador4);
+  setmark5(marcador5);
+}, []);
+
+const handleInput = () => (
+// requisição do backend
+);
+
 const ResultsRevision = () => (
   <div className="content">
     <div className="save-name">
@@ -43,11 +61,11 @@ const ResultsRevision = () => (
         <div>
           <h2>Marcadores</h2>
           <div className="Mark">
-            <input placeholder="Marcador 1" type="text" />
-            <input placeholder="Marcador 2" type="text" />
-            <input placeholder="Marcador 3" type="text" />
-            <input placeholder="Marcador 4" type="text" />
-            <input placeholder="Marcador 5" type="text" />
+            <input placeholder="Marcador 1" type="text" value={marcador1} onChange={(e) => handleInput(e, "mark1")} />
+            <input placeholder="Marcador 2" type="text" value={marcador2} onChange={(e) => handleInput(e, "mark2")} />
+            <input placeholder="Marcador 3" type="text" value={marcador3} onChange={(e) => handleInput(e, "mark3")} />
+            <input placeholder="Marcador 4" type="text" value={marcador4} onChange={(e) => handleInput(e, "mark4")} />
+            <input placeholder="Marcador 5" type="text" value={marcador5} onChange={(e) => handleInput(e, "mark5")} />
           </div>
         </div>
       </div>

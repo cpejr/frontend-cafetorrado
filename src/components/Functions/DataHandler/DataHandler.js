f354bed146b0dcacc505df274c04d58cc3113a8c
Gabriel Barros Mauad Santos <gabrielbms711@gmail.com>
2021-11-09 00:49:59 -0300

    persistir valor da automatica para manual

--- src/components/Functions/DataHandler/DataHandler.js
+++ src/components/Functions/DataHandler/DataHandler.js
@@ -1,25 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { socket } from '../../../index';
 
-const RealData = () => {
-  const [uppData, setUppData] = useState({
-    fields: {
-      BlkBegDaq: 3435973836,
-      BlkEndDaq: 3722304989,
-      BlkNotTrc: '',
-      BlkNt2Trc: '',
-      MdlAirOut: 0,
-      MdlAirScl: 0,
-      MdlDisErr: 5,
-      MdlDruOut: 0,
-      MdlGraScl: 0,
-      MdlInjOut: 0,
-      MdlRunCnt: 27677,
-      BchPrsScl: 0,
-      BchHumScl: 0,
-      BchTmpScl: 0,
-    },
-  });
+const RealData = ({ uppData, setUppData }) => {
   useEffect(() => {
     socket.on('realData', (data) => { setUppData(data); });
   }, []);
